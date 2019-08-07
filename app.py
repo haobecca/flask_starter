94369af57edddc9081d8e666cf4143f448c40094
haobecca <rebeccaanneh@mail.usf.edu>
2019-08-06 20:25:40 -0500

    initial commit

---
+++ app.py
@@ -0,0 +1,17 @@
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    name = request.args.get("name", "world")
+    return render_template("index.html", name=name)
+
+@app.route("/register", methods=["POST"])
+def register():
+    name = request.form.get("name")
+    dorm = request.form.get("dorm")
+    if not name or not dorm:
+        return "failure"
+    return render_template("success.html")
+    
